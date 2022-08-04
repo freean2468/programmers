1b53235a622407c282a77a28bda9229901fe1ba3
sensebecommon <freean2468@gmail.com>
2022-08-04 17:47:26 +0900

    add : https://school.programmers.co.kr/learn/courses/30/lessons/12932 풀이

---
+++ Lv.1/자연수 뒤집어 배열로 만들기/solution.py
@@ -0,0 +1,7 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12932
+def solution(n):
+    answer = []
+    while n > 0:
+        answer.append(n % 10)
+        n //= 10
+    return answer
