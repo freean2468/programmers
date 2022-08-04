a1db28ca166d01733b16ccb05f7af35c56b10c1f
sensebecommon <freean2468@gmail.com>
2022-08-04 10:37:38 +0900

    add : https://school.programmers.co.kr/learn/courses/30/lessons/12910 풀이

---
+++ Lv.1/나누어 떨어지는 숫자 배열/solution.py
@@ -0,0 +1,11 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12910
+def solution(arr, divisor):
+    answer = []
+    for a in arr:
+        if a % divisor == 0:
+            answer.append(a)
+            
+    if len(answer) == 0:
+        answer.append(-1)
+        
+    return sorted(answer)
