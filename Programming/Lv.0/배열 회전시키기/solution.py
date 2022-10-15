f967b562e7a1676f8e7e658fc46e19de280c205e
sensebecommon <freean2468@gmail.com>
2022-10-15 22:19:10 +0900

    add : https://school.programmers.co.kr/learn/courses/30/lessons/120844

---
+++ Programming/Lv.0/배열 회전시키기/solution.py
@@ -0,0 +1,3 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120844
+def solution(numbers, direction):
+    return numbers[1:] + [numbers[0]] if direction == "left" else [numbers[-1]] + numbers[:-1]
